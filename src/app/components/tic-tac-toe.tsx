4ea97ea51b5390f02b8d957ff17f1aa764ccaf56
Felipe Esteves <felipegouveiae@hotmail.com>
2023-04-08 13:15:05 -0300

    circle and X implemented

--- src/app/components/tic-tac-toe.tsx
+++ src/app/components/tic-tac-toe.tsx
@@ -7,33 +7,38 @@ export default () => {
             <div className="container">
 
 
-                <div className="row" style={{backgroundColor: 'red'}}>
-
-                    A
-                    {/*<div className="col border-right border-bottom">*/}
-
-                    {/*</div>*/}
-                    {/*<div className="col border-bottom">*/}
-
-                    {/*</div>*/}
-                    {/*<div className="col border-left border-bottom">*/}
-
-                    {/*</div>*/}
+                <div className="row">
+                    <div className="col border-right border-bottom">
+                        <div className="square"></div>
+                    </div>
+                    <div className="col border-bottom">
+                        <div className="circle">A</div>
+                    </div>
+                    <div className="col border-left border-bottom">
+                        C
+                    </div>
                 </div>
-                <div className="row" style={{backgroundColor: 'green'}}>
-                    B
-                    {/*<div className="col border-right border-bottom">*/}
-
-                    {/*</div>*/}
-                    {/*<div className="col border-bottom">*/}
-
-                    {/*</div>*/}
-                    {/*<div className="col border-left border-bottom">*/}
-
-                    {/*</div>*/}
+                <div className="row">
+                    <div className="col border-right border-bottom">
+                        D
+                    </div>
+                    <div className="col border-bottom">
+                        E
+                    </div>
+                    <div className="col border-left border-bottom">
+                        F
+                    </div>
                 </div>
-                <div className="row" style={{backgroundColor: 'blue'}}>
-                    C
+                <div className="row">
+                    <div className="col border-right">
+                        G
+                    </div>
+                    <div className="col">
+                        H
+                    </div>
+                    <div className="col border-left">
+                        I
+                    </div>
                 </div>
             </div>
         </div>
